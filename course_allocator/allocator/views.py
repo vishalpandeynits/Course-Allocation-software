7b94609b2f79671535f7ff1254e0b816c3aeb4ad
vishalpandeyvip <vishalpandeyviptsk@gmail.com>
2021-01-11 23:43:14 +0530

    folder structure initialised

--- course_allocator/allocator/views.py
+++ course_allocator/allocator/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def niranjan(request):
+    return render(request,'profile.html')
+    # return HttpResponse("<h1 style='color:red;'>I am niranjan</h1>")
