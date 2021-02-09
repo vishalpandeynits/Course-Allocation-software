86a635012e229e8ad4d2ba1709ac9a5a9270f37b
vishalpandeyvip <vishalpandeyviptsk@gmail.com>
2021-02-09 13:31:36 +0530

    dyanamic pdf for teachers added

--- course_allocator/allocator/models.py
+++ course_allocator/allocator/models.py
@@ -35,10 +35,10 @@ class Profile(models.Model):
 class Preference(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     preference_num = models.CharField(max_length=1,choices = [('1','1'),('2','2'),('3','3')])
+    ug_pg = models.CharField(max_length = 30,choices = graduate_choice)
     course_type = models.CharField(max_length=20,choices = course_type_choice)
     semester = models.CharField(max_length=3)
     course_name = models.CharField(max_length=100)
-    ug_pg = models.CharField(max_length = 30,choices = graduate_choice)
     
     def __str__(self):
         return self.user.first_name +" "+self.user.last_name
--- course_allocator/allocator/views.py
+++ course_allocator/allocator/views.py
@@ -4,6 +4,27 @@ from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from .models import *
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Table, TableStyle
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+from django.utils.timezone import now
+import io
+from django.http import FileResponse
+from django.contrib import messages
+
+def table_style():
+	return TableStyle([
+    ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+    ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+    ('LEFTPADDING', (0,0), (-1,-1), 0.1*inch),
+    ('RIGHTPADDING', (0,0), (-1,-1), 0.1*inch),
+    ('TOPPADDING', (0,0), (-1,-1), 0.1*inch),
+    ('BOTTOMPADDING', (0,0), (-1,-1), 0.1*inch),
+    ('FONTSIZE', (0,0), (-1,-1), 13),
+])
+def table_row():
+	return ['Preference','UG/PG','Semester','Subject']
 
 def home(request):
 	params = None
@@ -21,39 +42,51 @@ def home(request):
 		return render(request,'home.html',params)
 	return render(request,'home.html',params)
 
-def signup(request):
-	if request.user.is_authenticated:
-		return redirect(reverse('home'))
-	if request.method=='POST':
-		basicform= RegisterForm(request.POST)
-		advanceform = ProfileRegisterForm(request.POST)
-		if basicform.is_valid() and advanceform.is_valid():
-			user = basicform.save(commit=False)
-			advance_data = advanceform.save(commit=False)
-			user.username = user.email.split('@')[0] +'-' + user.email.split('@')[1].split('.')[0]
-			user.save()
-			advance_data.user = user
-			advance_data.save()
-			return redirect(reverse('login'))
-	else:
-		basicform= RegisterForm()
-		advanceform = ProfileRegisterForm()
-	params = {'form':basicform,'advance_form':advanceform}
-	return render(request,'registration/signup.html',params)
-
-def profile_page(request,username):
-	user = User.objects.get(username=username)
-	profile = Profile.objects.get(user=user)
-	params = {
-		'profile':profile,
-		'requested_user':user,
-	}
-	return render(request,'profile.html',params)
-
 @login_required
 def preference_page(request):
-	my_preferences=Preference.objects.filter(user=request.user)
-	if request.method=="POST":
+	user = request.user
+	profile = Profile.objects.get(user=user)
+	my_preferences=Preference.objects.filter(user=request.user).order_by('ug_pg')
+	if my_preferences.count()==6 and 'print' in request.POST:
+		core_courses = [list(i.values())[2:] for i in list(my_preferences.filter(course_type='core').values())]
+		elective_courses = [list(i.values())[2:] for i in list(my_preferences.filter(course_type='elective').values())]
+		for i in core_courses:
+			i.remove('core') # remove'core' tag from core course
+		for i in elective_courses:
+			i.remove('elective')# remove'elective' tag from elective course
+		core_courses.insert(0,table_row())
+		elective_courses.insert(0,table_row())
+		buffer = io.BytesIO()
+		page = canvas.Canvas(buffer)
+		page.setFont('Helvetica', 11)
+		page.drawString(0.6*inch,10.8*inch,f'Name: {user.first_name} {user.last_name} ')
+		page.drawString(0.6*inch,10.6*inch,f'Email: {user.email}')
+		page.drawString(0.6*inch,10.4*inch,f'Department: {profile.department}')
+		page.drawString(0.6*inch,10.2*inch,'Datetime: '+now().strftime('%d %b %Y %H:%M %p'))
+		page.drawInlineImage('https://upload.wikimedia.org/wikipedia/en/c/c6/NIT_Silchar_logo.png',6.4*inch,10*inch, width=100,height=100)
+		page.setFont('Helvetica', 22)
+		page.line(0.5*inch,10*inch,8*inch,10*inch)
+
+		page.drawString(3*inch,9.5*inch,"PREFERENCES")
+		page.drawCentredString(4*inch,9*inch,"Core Courses")
+		t=Table(core_courses,hAlign='CENTER')
+		t.setStyle(table_style())
+		page.setFont('Helvetica', 18)
+		t.wrapOn(page, 1.4*inch, 7.4*inch)
+		t.drawOn(page, 1.4*inch, 7.4*inch)
+
+		page.drawCentredString(4*inch,7*inch,"Elective Courses")
+		x = Table(elective_courses,hAlign='CENTER')
+		x.setStyle(table_style())
+		x.wrapOn(page,1.4*inch, 5.4*inch)
+		x.drawOn(page,1.4*inch, 5.4*inch)
+
+		page.showPage()
+		page.save()
+		buffer.seek(0)
+		return FileResponse(buffer, as_attachment=True, filename=f'{user.username}-preference.pdf')
+
+	elif request.method=="POST":
 		#core courses 
 		ug_pg1 = request.POST.get('ug_pg_cc_pre1')
 		semester1 = request.POST.get('semester_cc_pre1')
@@ -85,5 +118,36 @@ def preference_page(request):
 		Preference.objects.create(user=request.user,preference_num = '1',course_type='elective', semester = semester4, course_name = course4, ug_pg = ug_pg4)
 		Preference.objects.create(user=request.user,preference_num = '2',course_type='elective', semester = semester5, course_name = course5, ug_pg = ug_pg5)
 		Preference.objects.create(user=request.user,preference_num = '3',course_type='elective', semester = semester6, course_name = course6, ug_pg = ug_pg6)
+		messages.add_message(request,messages.SUCCESS, f'Your Preference submitted')
 	params={'my_preferences':my_preferences,}
 	return render(request,'select_preference.html',params)
+
+
+def signup(request):
+	if request.user.is_authenticated:
+		return redirect(reverse('home'))
+	if request.method=='POST':
+		basicform= RegisterForm(request.POST)
+		advanceform = ProfileRegisterForm(request.POST)
+		if basicform.is_valid() and advanceform.is_valid():
+			user = basicform.save(commit=False)
+			advance_data = advanceform.save(commit=False)
+			user.username = user.email.split('@')[0] +'-' + user.email.split('@')[1].split('.')[0]
+			user.save()
+			advance_data.user = user
+			advance_data.save()
+			return redirect(reverse('login'))
+	else:
+		basicform= RegisterForm()
+		advanceform = ProfileRegisterForm()
+	params = {'form':basicform,'advance_form':advanceform}
+	return render(request,'registration/signup.html',params)
+
+def profile_page(request,username):
+	user = User.objects.get(username=username)
+	profile = Profile.objects.get(user=user)
+	params = {
+		'profile':profile,
+		'requested_user':user,
+	}
+	return render(request,'profile.html',params)
